0ee4ff2c9775ce79ed476b6ce59834bdfaf55112
rachdk <rachael.d.kim@hotmail.com>
2020-03-22 23:55:18 -0700

    sign-up

---
+++ signIn.php
@@ -0,0 +1,37 @@
+<?php require_once("includes/config.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>RDKannel</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    
+
+</head>
+<body>
+
+    <div class = "signInContainer">
+        <div class = "column">
+            <div class = "header">
+                    <img src="assets/images/icons/MainLogo.png" title="logo" alt="Site logo">
+                    <h3>Sign Up</h3>
+                    <span>to continue to RDKannel</span>
+                </div>
+
+                <div class = "loginForm">
+                    <form action = "signUp.php">
+                    </form>
+
+                </div>
+
+                <a class = "signInMessage" href="signIn.php"> Already have an account? Sign in here!</a>
+        </div>
+    </div>
+
+</body>
+</html>
