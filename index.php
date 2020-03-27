662cf0f295082916d88257bd1f73b8c67b1f02ac
rachdk <rachael.d.kim@hotmail.com>
2020-03-27 14:07:13 -0700

    front page updated

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php require_once("includes/header.php"); ?>
 
+<div class = "videoSection">
+    <?php
 
-<?php
+    $subscriptionsProvider = new SubscriptionsProvider($con, $userLoggedInObj);
+    $subscriptionVideos = $subscriptionsProvider->getVideos();
 
-if(isset($_SESSION["userLoggedIn"]))
-{
-    echo "user is logged in as " . $userLoggedInObj->getName();
-}
-else{
-    echo "not logged in";
-}
-?>
+
+    $videoGrid = new VideoGrid($con, $userLoggedInObj->getUsername());
+
+    if(User::isLoggedIn() && sizeof($subscriptionVideos)>0) {
+        echo $videoGrid->create($subscriptionVideos, "Subscriptions", false);
+    }
+    echo $videoGrid->create(null,"Recommended",false);
+    ?>
+
+</div>
 
 <?php require_once("includes/footer.php"); ?>
